66cd2650953ac94ac6251ab19541156e5124db05
Niloc-Solidity <37029935+Niloc-Solidity@users.noreply.github.com>
2018-03-11 11:03:46 -0400

    Add files via upload

---
+++ C#_potionmagic/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=potionmagic-Web-Deploy.pubxml -p:Password=Pfji9KrlpnegBh51EocRYtB7md8lEksfxqMwEqc15k0uGbxnh8qDxeTspNzw
+
